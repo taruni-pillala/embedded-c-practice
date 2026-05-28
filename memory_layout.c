ec44da57e087543e83deccc036499c7c80b09875
taruni p <tarunip72@gmail.com>
2026-05-28 17:12:58 +0530

    add memory layout stack heap BSS data segments

--- memory_layout.c
+++ memory_layout.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
-
-// 1. GLOBAL VARIABLE (Initialized)
-// This lives in the "Data Section" (SRAM).
-// Its initial value (100) is stored in Flash and copied to SRAM when the MCU boots.
-int globally_tracked_heartbeats = 100;
-
-// 2. GLOBAL VARIABLE (Uninitialized)
-// This lives in the "BSS Section" (SRAM).
-// The microcontroller automatically resets this to 0 at startup.
-int patient_alert_counter;
-
-// 3. CONSTANT VARIABLE
-// This lives permanently in the "Read-Only / Flash Memory".
-// It can never be changed by the code while running.
-const int DEVICE_ID = 5542;
-
-void my_medical_function(void)
+#include <stdlib.h>
+// data segment-global variable WITH initial value
+int global_with_value = 42;
+// BSS segment-global variable without the inital value
+int global_no_value;
+int main()
 {
-    // 4. LOCAL VARIABLE
-    // This lives in the "Stack Section" (SRAM).
-    // It is created dynamically when this function starts, and destroyed the moment the function ends.
-    int temporary_sensor_reading = 45;
+    // Stack-local variable inside function
+    int local_variable = 100;
+    // heap-dynamically allocated memory
+    int *heap_variable = (int *)malloc(sizeof(int));
+    *heap_variable = 200;
+    // static- stays in DATA segment, remembered between calls
+    static int stactic_variable = 50;
 
-    printf("Temporary Reading inside function: %d\n", temporary_sensor_reading);
-}
+    printf("==MEMORY LAYOUT EXAMPLE==\n\n");
 
-int main(void)
-{
-    // Calling our function to look at local variables
-    my_medical_function();
+    printf("DATA SEGMENT:\n");
+    printf("global_with_value: %d\n", global_with_value);
+    printf("Address: %p\n\n", (void *)&global_with_value);
 
-    // Printing values just to verify they exist
-    printf("Device ID (Flash): %d\n", DEVICE_ID);
-    printf("Heartbeats (Data Segment): %d\n", globally_tracked_heartbeats);
-    printf("Alert Counter (BSS Segment): %d\n", patient_alert_counter);
+    printf("BSS SEGMENT:\n");
+    printf("global_no_value: %d\n", global_no_value);
+    printf("Address: %p\n\n", (void *)&global_no_value);
 
+    printf("STACK:\n");
+    printf("local_variable: %d\n", local_variable);
+    printf("Address: %p\n\n", (void *)&local_variable);
+
+    printf("Data segment(static):\n");
+    printf("stactic_variable: %d\n", stactic_variable);
+    printf(" Address: %p\n\n", (void *)&static_variable);
+    // always free heap
+    free(heap_variable);
     return 0;
 }
